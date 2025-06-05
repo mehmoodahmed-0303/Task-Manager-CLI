c6a859e00c73a17275a8a60995689fd5fec941fa
Mehmood <Mehmood3980350@gmail.com>
2025-06-06 01:28:08 +0500

    Initialized users.json and tasks.json with startup function

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-	print("Task Manager Started.")
+import json
+import os
 
+USERS_FILE = 'users.json'
+TASKS_FILE = 'tasks.json'
+
+def initialize_files():
+	if not os.path.exists(USERS_FILE):
+		with open(USERS_FILE, 'w') as file:
+			json.dump({}, file)
+	if not os.path.exists(TASKS_FILE):
+		with open(TASKS_FILE, 'w') as file:
+			json.dump([], file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	initialize_files()
+	print("Task Manager Started.")
 
 if __name__ == '__main__':
 	main()
