ea0204a8892ea489f14bcdf46c36ee1ea22350f7
Mehmood <Mehmood3980350@gmail.com>
2025-06-07 02:51:53 +0500

    Enhance user registration with password strength

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
 import os
+import bcrypt
+import re
+from datetime import datetime
 
 USERS_FILE = 'users.json'
 TASKS_FILE = 'tasks.json'
@@ -12,10 +15,59 @@ def initialize_files():
 		with open(TASKS_FILE, 'w') as file:
 			json.dump([], file)
 
+def is_strong_password(password):
+	if len(password) < 8:
+		return False, "Password must be atleast 8 characters"
+	if not re.search(r"[A-Z]", password):
+		return False, "Password must contain at least one uper case latter"
+	if not re.search(r"[a-z]", password):
+		return False, "Password must contain at least one lowercase latter"
+	if not re.search(r"[0-9]", password):
+		return False, "Password must contain at least one number."
+	if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+		return False, "Password must contain at least one Special character"
+	return True, ""
+def is_valid_email(email):
+	if not email:
+		return True
+	return bool(re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email))
 
+def register_user():
+	try:
+		with open(USERS_FILE, 'r') as file:
+			users = json.load(file)
+		while True:
+			username = input("Enter user name (or cancel to exit): ").strip().lower()
+			if username == 'cancel':
+				print("Registration canceled.")
+				return
+			if not username:
+				print("User name can not be empty")
+				continue
+			if username in users:
+				print("User name already exists")
+				continue
+			break
+		while True:
+			password = input("Enter password: ").strip()
+			is_strong, message = is_strong_password(password)
+			if not is_strong:
+				print(message)
+				continue
+			break
+		email = input("Enter email(optional, press enter to skipp): ")
+		if not is_valid_email(email):
+			print("Invalid email format. Skipping email")
+			email = ""
 
+		hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+		users[username] = {'password': hashed.decode(), 'email':email, 'registered_date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
 
-
+		with open(USERS_FILE, 'w') as file:
+			json.dump(users, file, indent=4)
+		print(f"User {username} registered successfuly.")
+	except Exception as e:
+		print(f"Error: {e}")
 
 
 
@@ -27,7 +79,17 @@ def initialize_files():
 
 def main():
 	initialize_files()
-	print("Task Manager Started.")
-
+	while True:
+		print("\nTask Manager Menu")
+		print("1. Register")
+		print("2. Exit")
+		choice = input("Enter choice (1-2): ").strip()
+		if choice == '1':
+			register_user()
+		elif choice == '2':
+			print("Exiting...")
+			break
+		else:
+			print("Invalid choice. please choose 1-2")
 if __name__ == '__main__':
 	main()
